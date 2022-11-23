9b3e7d60fa19ca02bac2237d03e8054efdb9736f
Prashant <114965028+prashant9569@users.noreply.github.com>
2022-11-23 10:57:38 +0000

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt install default-jdk
+sudo apt install python3
+sudo apt install pyhton2
+sudo apt install ruby
+sudo apt install goland
+sudo apt install g++
+
+
